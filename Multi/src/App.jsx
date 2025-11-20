96bf97a20a46c0269012c626f713fe7b9d598a43
AMAN ANAND <amananandroy767@gmail.com>
2025-11-20 15:53:03 +0530

    Multiplication without using State (using Variable)

---
+++ Multi/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [Value, setValue] = useState(1)
+  // const [MultipliedValue, setMultipliedValue] = useState(1)
+    let MultipliedValue = Value * 5
+
+  
+  const multiplybyfive = () => {
+    // setMultipliedValue(Value * 5)
+    setValue(Value + 1)
+  }
+
+  return (
+    <>
+      <h1>Main Value:{Value}</h1>
+      <button onClick={multiplybyfive}
+      >Click to Multiply by 5</button>
+      <h2>Multiplied Value: {MultipliedValue}</h2>
+    </>
+  )
+}
+
+export default App
